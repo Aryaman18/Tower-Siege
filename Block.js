77ed43f4c9667a5a1aa451ef2e2c603d34ad48d9
Aryaman18 <79416811+Aryaman18@users.noreply.github.com>
2021-10-08 10:30:55 -0700

    Add files via upload

--- Block.js
+++ Block.js
@@ -5,6 +5,7 @@ class Block{
             friction :0.0,
             // set the isStatic property 
             isStatic : false
+            this.Visibility = 255
         }
         
         this.body = Bodies.rectangle(x, y, width, height, options);
@@ -20,6 +21,11 @@ class Block{
         rotate(angle);
         rectMode(CENTER);
         rect(0,0,this.width, this.height);
+       
+      this.Visibility = this.Visibility-5;
+      tint(255,this.Visibility);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50)
+
         pop();
       }
 }
